967cc3204d8250833c1464b733393f62d112d2e5
Javel Wilson <javelawilson@gmail.com>
2023-11-01 19:30:57 -0400

    Implement basic camera usage

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import {Camera} from 'expo-camera';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
 
-export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+const Page = () => {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
       </View>
+    );
+  }
+
+  return (
+    <View className="flex-1">
+      <Camera className="flex-1"></Camera>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+export default Page;
