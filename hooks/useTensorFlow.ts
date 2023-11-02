aac9dc38c924497119cbce57f40aae42bfd2c00a
Javel Wilson <javelawilson@gmail.com>
2023-11-02 02:21:30 -0400

    Add custom hook to load TensorFlow model

--- hooks/useTensorFlow.ts
+++ hooks/useTensorFlow.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import * as tf from '@tensorflow/tfjs';
 
 export const useTensorFlowLoaded = () => {
@@ -19,4 +19,25 @@ export const useTensorFlowLoaded = () => {
   }, []);
 
   return isLoaded;
+};
+
+type ModelKind = {
+  load: () => Promise<any>;
+};
+
+export const useTensorFlowModel = (modelKind: ModelKind) => {
+  const [model, setModel] = useState(null);
+
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    setModel(null);
+    modelKind.load().then((model) => {
+      if (isMounted.current) {
+        setModel(model);
+      }
+    });
+  }, [modelKind]);
+
+  return model;
 };
